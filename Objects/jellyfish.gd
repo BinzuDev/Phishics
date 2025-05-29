34031e3cc6db57f3a1e8d471fa4a6c5269ce9497
HamHuz <sharky303030@gmail.com>
2025-05-29 19:33:50 +0400

    h

--- Objects/jellyfish.gd
+++ Objects/jellyfish.gd
@@ -1,14 +1,30 @@
-extends Area3D
+extends Node3D
+
+@export var speed = 5
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	#store y axis
+	%AnimationJellyfish.play("Jellyfish")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	pass
+	#makes him move
+	%PathFollowJellyfish.progress += speed * _delta
+	
+	#sin
+	#var time = 0
+	#var amplitude = 1
+	#var frequency = 2
+	#time += _delta
+	#position.y = sin(time * frequency) * amplitude
+	  
+	#checks amount reached in path
+	if %PathFollowJellyfish.progress_ratio >= 0.3:
+		print("Reached over 30% of the path")
+	
 
 
 func _on_body_entered(body: Node3D) -> void:
