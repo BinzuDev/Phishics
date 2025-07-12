831c1e37636386ade060d1f3c0030eeaa832d1a9
HamHuz <sharky303030@gmail.com>
2025-07-12 10:39:55 +0400

    tutorial

---
+++ Objects/goal.gd
@@ -0,0 +1,19 @@
+extends Area3D
+
+@export var nextLevel = ("res://Levels/World.tscn")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_body_entered(body: Node3D) -> void:
+	if body is player:
+		print("GOAL!")
+		get_tree().change_scene_to_file(nextLevel)
