3fc69341142c524bdefe06600ef06a82ffbe5f2c
HamHuz <sharky303030@gmail.com>
2025-08-12 20:28:05 +0400

    h

---
+++ Objects/strike_zone.gd
@@ -0,0 +1,39 @@
+extends Node3D
+var touched: bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$STRIKEsprite.visible = false #make sure its invisable at start
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func apply_force_to_rigidbodies():
+	var node_center = $Pins.global_transform.origin #get center of node
+	var force_range = 1.1
+	var node_3d = $Pins # Get the Node3D child
+	for child in node_3d.get_children():
+		if child is RigidBody3D:
+				var direction = child.global_transform.origin - node_center
+				child.apply_central_impulse(direction.normalized() * force_range) #apply force opposite of the center
+
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body is player and not touched:
+		
+		#confirm touched
+		touched = true
+		
+		#trick
+		ScoreManager.give_points(500, 2, true, "STRIKE!", "rare", false)
+		ScoreManager.play_trick_sfx("rare")
+		print("strike!")
+		
+		apply_force_to_rigidbodies() #func for strike
+		
+		#effects
+		$strikeSFX.play() #sfx
+		%strikeAnimation.play("STRIKE") #animation
