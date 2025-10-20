cb39e53079485247524ab5cf8c4ca930eb405e67
BinzuDev <xddrouin3@gmail.com>
2025-10-20 06:45:50 -0400

    binzu coral

--- Objects/coral_clump.gd
+++ Objects/coral_clump.gd
@@ -2,20 +2,11 @@ extends Node3D
 var touched = false
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 
 func _coral_spread():
 	for child in $corals.get_children():
 		if child is RigidBody3D:
-			child.collision_mask = child.collision_mask | 1 #enables collison with the world so we can plant it in sand
+			child.set_collision_mask_value(1, true)#enables collison with the world so we can plant it in sand
 			child.gravity_scale = 0.01
 
 
