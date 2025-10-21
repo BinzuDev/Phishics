e92dfe7babd7d815944a0d5c38c55efb2461722f
HamHuz <sharky303030@gmail.com>
2025-10-21 19:29:42 +0400

    re purposed tube coral
    I also added a brain coral texture jic

--- Objects/coral_clump.gd
+++ Objects/coral_clump.gd
@@ -1,24 +1,24 @@
 extends Node3D
-var touched = false
+#var touched = false
 
 
 
-func _coral_spread():
-	for child in $corals.get_children():
-		if child is RigidBody3D:
-			child.set_collision_mask_value(1, true)#enables collison with the world so we can plant it in sand
-			child.gravity_scale = 1
+#func _coral_spread():
+	#for child in $corals.get_children():
+		#if child is RigidBody3D:
+			#child.set_collision_mask_value(1, true)#enables collison with the world so we can plant it in sand
+			#child.gravity_scale = 1
 
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	if body is player and not touched:
-		_coral_spread()
-		touched = true
-		
-		#trick
-		ScoreManager.give_points(200, 1, true, "DESTRUCTION")
-		ScoreManager.update_freshness(self)
-		ScoreManager.play_trick_sfx("uncommon")
-		
+	#if body is player and not touched:
+		#_coral_spread()
+		#touched = true
+		#
+		##trick
+		#ScoreManager.give_points(200, 1, true, "DESTRUCTION")
+		#ScoreManager.update_freshness(self)
+		#ScoreManager.play_trick_sfx("uncommon")
+		$corals/AnimationPlayer.play("coral_wiggle")
 		print("touched")
