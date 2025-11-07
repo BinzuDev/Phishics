58e9efdde28396d6ab8c954ab50efc4fd5561294
HamHuz <sharky303030@gmail.com>
2025-11-08 01:58:48 +0400

    island shit im tired im going to bed its 2am

--- Objects/rotating_obj.gd
+++ Objects/rotating_obj.gd
@@ -1,12 +1,43 @@
 extends MeshInstance3D
 
+#script for rotating objects
 
-# Called when the node enters the scene tree for the first time.
+@export var xRotation : bool = false
+@export var xRandom : bool = false
+@export var yRotation : bool = false
+@export var yRandom : bool = false
+
+var randomOfX = 1
+var randomOfY = 1
+
+
 func _ready() -> void:
-	pass # Replace with function body.
+	#randomizes x and y rotation
+	if xRandom:
+		randomOfX = randi_range(1, 2)
+	
+	if yRandom:
+		randomOfY = randi_range(1, 2)
+	
+	%AnimationPlayer.play("Floating")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:\
-	rotation.x += 0.01
-   
+func _process(_delta: float) -> void:
+	
+	
+	###
+	if yRotation and randomOfY == 1:
+		rotation.y += 0.01
+	elif yRotation and randomOfY == 2:
+		rotation.y -= 0.01
+	###
+	
+	###
+	if xRotation and randomOfX == 1:
+		rotation.x += 0.01
+	elif xRotation and randomOfX == 2:
+		rotation.x -= 0.01
+	###
+
+
+  
