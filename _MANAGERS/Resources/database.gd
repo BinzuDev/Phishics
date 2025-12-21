6366edd36adb517e58dd0e06494f4eaa6d5f0f5a
HamHuz <sharky303030@gmail.com>
2025-12-21 12:50:38 +0400

    get func

--- _MANAGERS/Resources/database.gd
+++ _MANAGERS/Resources/database.gd
@@ -2,3 +2,16 @@ class_name Database extends Resource
 
 
 @export var skins: Array[SkinData]
+
+
+
+
+func get_skin(id: int):
+	
+	if id > skins.size():
+		printerr("skin out of bounds")
+		return
+	
+	print("get skin: ", skins[id])
+	return skins[id]
+	
